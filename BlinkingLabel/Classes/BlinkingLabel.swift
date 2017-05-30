9c25a527f2be0c4abb659f0d231419a0b3d98a45
Ruben DJAN <djanruben@mac.com>
2017-05-30 20:41:14 +0200

    update

--- BlinkingLabel/Classes/BlinkingLabel.swift
+++ BlinkingLabel/Classes/BlinkingLabel.swift
@@ -2,8 +2,8 @@ import UIKit
 
 public class BlinkingLabel : UILabel {
     public func startBlinking() {
-        let options : UIViewAnimationOptions = .Repeat | .Autoreverse
-        UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
+        let options : UIViewAnimationOptions = .repeat
+        UIView.animate(withDuration : 0.25, delay:0.0, options:options, animations: {
             self.alpha = 0
         }, completion: nil)
     }
